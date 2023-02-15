969cbead256265f801e2fad3bcbb570be86298f9
Herman Jangsett Mostein <herman.mostein@hotmail.com>
2023-02-15 17:05:59 +0100

    Improve testing grounds

--- DRLPSO/test_results.py
+++ DRLPSO/test_results.py
@@ -10,12 +10,11 @@ import supersuit as ss
 
 def main(agent, env_parameter_dict = None):
 
-    env_parameter_dict = env_parameter_dict if env_parameter_dict else {
+    env_parameter_dict = {
         "num_particles": 50,
         "dimensions": 2,
         "iterations": 100,
-        "reward_function": final_score,
-        "prod_mode": True,
+        "reward_function": final_score
     }
     hp_dict = {
         "prod_mode": True,
@@ -33,7 +32,7 @@ def main(agent, env_parameter_dict = None):
     env = Env(
         agents=agents, reward_function=env_parameter_dict["reward_function"],
         dimensions=env_parameter_dict["dimensions"], iterations=env_parameter_dict["iterations"],
-        prod_mode=env_parameter_dict["prod_mode"], use_agent=False)
+        prod_mode=parameter_dict["prod_mode"], use_agent=True)
 
     agent_env = aec_to_parallel(env)
     agent_env = ss.pettingzoo_env_to_vec_env_v1(agent_env)
@@ -51,9 +50,19 @@ def main(agent, env_parameter_dict = None):
     if (hp_dict["prod_mode"]):
         agent.save_model()
 
+    env = Env(
+        agents=agents, reward_function=env_parameter_dict["reward_function"],
+        dimensions=env_parameter_dict["dimensions"], iterations=env_parameter_dict["iterations"],
+        prod_mode=parameter_dict["prod_mode"], use_agent=False)
+
     pso_obj, pso_imp = run_pso(50, env=env, env_parameter_dict = env_parameter_dict)
+
+    env = Env(
+        agents=agents, reward_function=env_parameter_dict["reward_function"],
+        dimensions=env_parameter_dict["dimensions"], iterations=env_parameter_dict["iterations"],
+        prod_mode=parameter_dict["prod_mode"], use_agent=True)
     drlpso_obj, drlpso_imp = test_agent(50, agent, env, env_parameter_dict = env_parameter_dict)
-    
+
 
 main(None)
     
